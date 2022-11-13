bc51a01ae581dd015acb5de7dd2a7d6cf16cc80b
Alejandro Campos <acamposcar@gmail.com>
2022-11-13 19:00:15 +0100

    Change stroke color to black

--- src/components/ui/loading.tsx
+++ src/components/ui/loading.tsx
@@ -17,7 +17,7 @@ const Spinner = () => {
       height="50"
       viewBox="0 0 44 44"
       xmlns="http://www.w3.org/2000/svg"
-      stroke="black"
+      stroke="white"
     >
       <g fill="none" fillRule="evenodd" strokeWidth="2">
         <circle cx="22" cy="22" r="1">
