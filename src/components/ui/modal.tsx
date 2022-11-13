88312ae59719fa0d953f80a7ceaa14ee04a32d40
Alejandro Campos <acamposcar@gmail.com>
2022-11-13 18:59:34 +0100

    Create modal

---
+++ src/components/ui/modal.tsx
@@ -0,0 +1,101 @@
+import { Fragment, useRef } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+interface Props {
+  children: React.ReactNode
+  title: string
+  description?: string
+  isOpen: boolean
+  buttonText: string
+  setIsOpen: (isOpen: boolean) => void
+  handleClick: () => void
+}
+
+const Modal = ({
+  isOpen,
+  setIsOpen,
+  children,
+  title,
+  buttonText,
+  description,
+  handleClick
+}: Props) => {
+  const cancelButtonRef = useRef(null)
+
+  return (
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        initialFocus={cancelButtonRef}
+        onClose={setIsOpen}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative max-h-[80vh] transform overflow-y-auto rounded-lg bg-zinc-800 text-left text-zinc-100 shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className=" px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 "
+                      >
+                        {title}
+                      </Dialog.Title>
+                      {description && (
+                        <div className="mt-2">
+                          <p className="text-sm text-zinc-300">{description}</p>
+                        </div>
+                      )}
+                      <div className="mt-2">{children}</div>
+                    </div>
+                  </div>
+                </div>
+                <div className=" items-center gap-2 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={handleClick}
+                  >
+                    {buttonText}
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    onClick={() => setIsOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
+
+export default Modal
