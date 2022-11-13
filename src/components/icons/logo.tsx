8541e6a03b98a86ee10b2b215da7262bcd2c5445
Alejandro Campos <acamposcar@gmail.com>
2022-11-13 12:14:57 +0100

    Add icons

---
+++ src/components/icons/logo.tsx
@@ -0,0 +1,84 @@
+const Logo = ({ className = '', ...props }) => (
+  <svg
+    width="60"
+    height="41"
+    viewBox="0 0 54 37"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
+    <path
+      d="M11.1441 32.2125L1.23607 18.5752L11.1441 4.93798L27.1757 10.147L27.1757 27.0035L11.1441 32.2125Z"
+      fill="black"
+      stroke="url(#paint0_radial_104_52)"
+      strokeWidth="2"
+    />
+    <path
+      d="M17.1441 32.2125L7.23607 18.5752L17.1441 4.93798L33.1757 10.147L33.1757 27.0035L17.1441 32.2125Z"
+      fill="black"
+      stroke="url(#paint1_radial_104_52)"
+      strokeWidth="2"
+    />
+    <path
+      d="M23.1441 32.2125L13.2361 18.5752L23.1441 4.93798L39.1757 10.147L39.1757 27.0035L23.1441 32.2125Z"
+      fill="black"
+      stroke="url(#paint2_radial_104_52)"
+      strokeWidth="2"
+    />
+    <path
+      d="M30.863 34.4109L19.4673 18.7259L30.863 3.04104L49.3017 9.03214L49.3017 28.4198L30.863 34.4109Z"
+      fill="black"
+      stroke="url(#paint3_radial_104_52)"
+      strokeWidth="2"
+    />
+    <defs>
+      <radialGradient
+        id="paint0_radial_104_52"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(-9.83694 18.5752) scale(51.8488 51.8488)"
+      >
+        <stop stopColor="#00D1FF" />
+        <stop offset="1" stopColor="#8F00FF" />
+      </radialGradient>
+      <radialGradient
+        id="paint1_radial_104_52"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(-3.83694 18.5752) scale(51.8488 51.8488)"
+      >
+        <stop stopColor="#00D1FF" />
+        <stop offset="1" stopColor="#8F00FF" />
+      </radialGradient>
+      <radialGradient
+        id="paint2_radial_104_52"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(2.16306 18.5752) scale(51.8488 51.8488)"
+      >
+        <stop stopColor="#00D1FF" />
+        <stop offset="1" stopColor="#8F00FF" />
+      </radialGradient>
+      <radialGradient
+        id="paint3_radial_104_52"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(7.03447 18.7259) scale(59.0161)"
+      >
+        <stop stopColor="#00D1FF" />
+        <stop offset="1" stopColor="#8F00FF" />
+      </radialGradient>
+    </defs>
+  </svg>
+)
+
+export default Logo
