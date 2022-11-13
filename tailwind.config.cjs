40e0637b9975adeb74c6a2d708a71fa574916085
Alejandro Campos <acamposcar@gmail.com>
2022-11-13 13:49:43 +0100

    Add extra screen sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xxs: '430px',
+      xs: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
+
     extend: {}
   },
   plugins: []
