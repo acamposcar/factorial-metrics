343b676fd1654e5fd02510d70182fe87ae911e40
Alejandro Campos <acamposcar@gmail.com>
2022-11-17 19:02:27 +0100

    Pass setSelectedMetric as prop to VisualizeMetric

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,7 +2,6 @@ import { type NextPage } from 'next'
 import Head from 'next/head'
 import { useSession } from 'next-auth/react'
 
-import { trpc } from '../utils/trpc'
 import { useRouter } from 'next/router'
 import Loading from '../components/ui/loading'
 import MetricsList from '../components/metrics-list'
@@ -30,7 +29,7 @@ const Dashboard: NextPage = () => {
       </Head>
       <section className="height-screen-helper mx-auto flex border-t border-zinc-400">
         <div className="flex flex-1 flex-col lg:flex-row">
-          <div className="lg:height-screen-helper h-[40vh] overflow-y-scroll px-2 py-4 lg:w-[250px]  lg:px-4 lg:py-6">
+          <div className="lg:height-screen-helper h-[40vh] overflow-y-auto px-2 py-4 lg:w-[300px] lg:overflow-y-scroll lg:px-4 lg:py-6">
             <CreateMetric setSelectedMetric={setSelectedMetric} />
             <div className=" ">
               <MetricsList
@@ -45,12 +44,13 @@ const Dashboard: NextPage = () => {
               <VisualizeMetric
                 metricId={selectedMetric.id}
                 metricName={selectedMetric.name}
+                setSelectedMetric={setSelectedMetric}
               />
             )}
             {!selectedMetric && (
               <div className="flex h-full flex-col items-center justify-center">
                 <h1 className="text-2xl font-bold text-zinc-400">
-                  Select a metric to visualize
+                  Select or create a metric
                 </h1>
               </div>
             )}
