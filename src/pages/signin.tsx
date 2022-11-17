424d0815bc4ab8afc6c6d62c188ad2a462daaf5a
Alejandro Campos <acamposcar@gmail.com>
2022-11-17 19:01:24 +0100

    Add logo and increase text size

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -4,6 +4,7 @@ import GitHub from '../components/icons/github'
 import { signIn, useSession } from 'next-auth/react'
 import Head from 'next/head'
 import LoadingSpinner from '../components/ui/loading-spinner'
+import Logo from '../components/icons/logo'
 
 const SignIn = () => {
   const [loading, setLoading] = useState(false)
@@ -29,12 +30,14 @@ const SignIn = () => {
       </Head>
       <section className="height-screen-helper flex items-center justify-center pb-24">
         <div className="flex w-80 max-w-lg flex-col justify-between p-3 ">
-          <div className="flex justify-center pb-12 ">
-            <h1 className="text-3xl font-bold">Sign in to Metrics</h1>
+          <div className="flex flex-col items-center justify-center pb-12 ">
+            <Logo width={70} height={70} />
+
+            <h1 className="mt-8 text-3xl font-bold">Sign in to Metrics</h1>
           </div>
           <button className="btn btn-secondary btn-flex" onClick={handleSignIn}>
             {loading ? <LoadingSpinner /> : <GitHub />}
-            <span className="ml-2">Continue with Github</span>
+            <span className="ml-2 text-lg">Continue with Github</span>
           </button>
         </div>
       </section>
